de2c58920fc0e4508fc9fd6a47d850a686ddfc34
Alfredo Luco <alfredoluco@MacBook-Pro-de-Alfredo.local>
2016-03-09 21:29:28 -0300

    Facebook

---
+++ Eventyk/Bridging-header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-header.h
+//  Eventyk
+//
+//  Created by Alfredo Luco on 09-03-16.
+//  Copyright © 2016 Innovarco. All rights reserved.
+//
+
+#ifndef Bridging_header_h
+#define Bridging_header_h
+
+
+#endif /* Bridging_header_h */
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
--- Eventyk/ViewController.swift
+++ Eventyk/ViewController.swift
@@ -11,12 +11,13 @@ import Alamofire
 import pop
 import MBProgressHUD
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController,UITextFieldDelegate,FBSDKLoginButtonDelegate {
 
     //Outlets
     @IBOutlet var userTextField: UITextField!
     @IBOutlet var passTextField: UITextField!
     
+    @IBOutlet var fbLogin: FBSDKLoginButton!
     //global variables
     var hud: MBProgressHUD = MBProgressHUD()
     
@@ -37,6 +38,17 @@ class ViewController: UIViewController,UITextFieldDelegate {
         hud.mode = .Indeterminate
         hud.labelText = "Cargando"
         hud.hidden = true
+        
+        //Facebook 
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+            returnUserData()
+//            self.performSegueWithIdentifier("loginSigninSegue", sender: self)
+        }else{
+            self.fbLogin.readPermissions = ["public_profile", "email", "user_friends"]
+            self.fbLogin.delegate = self
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -71,6 +83,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
     func textFieldDidEndEditing(textField: UITextField) {
         textField.resignFirstResponder()
     }
+    
     
     //MARK: - IBActions
     
@@ -109,6 +122,52 @@ class ViewController: UIViewController,UITextFieldDelegate {
     func hideKeyboard(){
         self.view.endEditing(true)
     }
+    
+    //MARK: - Facebook Delegate
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        //logout
+    }
 
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"id,name,email"])
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as! NSString
+                print("User Email is: \(userEmail)")
+            }
+        })
+    }
 }
 
