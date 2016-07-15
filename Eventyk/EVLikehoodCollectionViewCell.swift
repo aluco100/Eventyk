1ac01aaa4b8d0b3fea23b31ad95d8bf1d275b2b7
Alfredo Luco <alfredoluco@MacBook-Pro-de-Alfredo.local>
2016-07-14 22:05:45 -0400

    likehood view init

---
+++ Eventyk/EVLikehoodCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EVLikehoodCollectionViewCell.swift
+//  Eventyk
+//
+//  Created by Alfredo Luco on 14-07-16.
+//  Copyright © 2016 Innovarco. All rights reserved.
+//
+
+import UIKit
+
+class EVLikehoodCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var likehoodImage: UIImageView!
+    @IBOutlet var likehoodTitle: UILabel!
+    
+}
